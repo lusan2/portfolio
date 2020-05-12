fead7c88e689eb403d35b8af92eb0b294d6a1887
Lucía A. Sánchez H <sanchezhla@gmail.com>
2020-05-12 21:49:51 +0200

    Updates

---
+++ precache-manifest.755b30df55e2c0fc11bf01453a232dc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8002cc9a288b0d1854ca700143fa9d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c58d46995422b7e7b44",
+    "url": "/portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "680dae43ea1524e55f2f",
+    "url": "/portfolio/static/css/main.6098338b.chunk.css"
+  },
+  {
+    "revision": "2c58d46995422b7e7b44",
+    "url": "/portfolio/static/js/2.bbe62bbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.bbe62bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680dae43ea1524e55f2f",
+    "url": "/portfolio/static/js/main.139b1a1f.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "204365ad84c02d8187723485bef31fb0",
+    "url": "/portfolio/static/media/Logo.204365ad.svg"
+  },
+  {
+    "revision": "97ce31c8546f65bd1e25afbbf86ec1de",
+    "url": "/portfolio/static/media/linkedin.97ce31c8.svg"
+  },
+  {
+    "revision": "3e6c8801150195fd85c5b18adb82a03d",
+    "url": "/portfolio/static/media/stars.3e6c8801.svg"
+  },
+  {
+    "revision": "631cd5664eb1d4a27681a21ca4ad6126",
+    "url": "/portfolio/static/media/twitter.631cd566.svg"
+  }
+]);
